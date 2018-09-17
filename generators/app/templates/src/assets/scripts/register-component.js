cb34053d597b13f6500c2a1cee128a3c7cd4bf82
q-jason <596908546@qq.com>
2018-09-17 09:21:16 +0800

    细节优化

--- generators/app/templates/src/assets/scripts/register-component.js
+++
@@ -1,3 +0,0 @@
-// 自动引入 components 目录下的 js
-let req = require.context('../../components', true, /\.js$/i);
-req.keys(req).map(req);
--- generators/app/templates/src/main.js
+++ generators/app/templates/src/main.js
@@ -1,5 +1,7 @@
-// 自动引入 components 文件下的所有文件
-import './assets/scripts/register-component';
-
 // 网站中的公共样式
 import './assets/styles/global.scss';
+
+// 引入 components
+let req = require.context('./components', true, /\.js$/i);
+req.keys(req).map(req);
+
